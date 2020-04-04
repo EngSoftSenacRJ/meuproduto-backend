85ef5bfde4f4de9330c9651a5b7393659f701bb5
rderoci <rderociml@gmail.com>
2020-04-04 12:09:22 -0300

    SPRINT 4                                                      - Adicionado verificação se Produto já existe com o mesmo nome. - Realizado ajuste no cadastro da associação Produto x Loja
    Signed-off-by: Romulo Rocha <rrocha@hpe.com>

--- src/main/java/senac/edu/engsoft/meuproduto/model/dto/LojaProdutoDTO.java
+++ src/main/java/senac/edu/engsoft/meuproduto/model/dto/LojaProdutoDTO.java
@@ -3,7 +3,6 @@ package senac.edu.engsoft.meuproduto.model.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -11,13 +10,10 @@ import javax.validation.constraints.NotNull;
 public class LojaProdutoDTO {
 
 	@NotNull
-	@NotBlank(message = "idLoja é obrigatório")
 	private Long idLoja;
 	@NotNull
-	@NotBlank(message = "idProduto é obrigatório")
 	private Long idProduto;
 	@NotNull
-	@NotBlank(message = "preco é obrgatório")
 	private Double preco;
 
 }
